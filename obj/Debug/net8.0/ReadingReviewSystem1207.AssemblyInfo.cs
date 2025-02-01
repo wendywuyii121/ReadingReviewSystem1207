596de4c8c579259933aacdac157b752d43f91f5c
wendy <wendywuyii121@gmail.com>
2025-02-01 17:17:41 +0800

    上傳目前版本：包含完整設定、文件與環境調整，作為回復基準

--- obj/Debug/net8.0/ReadingReviewSystem1207.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReadingReviewSystem1207.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadingReviewSystem1207")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028543d8e5722c3c99693d9959bb4274c74a1ea0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529c4070dba7b41e8d84d7a9f6042258c0c41dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadingReviewSystem1207")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadingReviewSystem1207")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
